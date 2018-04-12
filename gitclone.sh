e703098e95158a2caa6902af188a80dbb19e2b81
Craig Laprade <craig9080@gmail.com>
2018-04-11 20:23:28 -0400

    Bug fixes

--- gitclone.sh
+++ gitclone.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-rm -r /admin
+rm -r /tmp/admin
+cd /tmp
 ssh-agent bash -c 'ssh-add /SSH_Priv.key; git clone ssh://admin@172.99.1.2:/repos/admin.git'
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 chdir('/');
 $input = system('bash ./gitclone.sh');
 chdir('var/www/html');
-$files = glob('/admin/*.{sh}', GLOB_BRACE);
+$files = glob('/tmp/admin/*.{sh}', GLOB_BRACE);
 foreach($files as $file) {
  $output = shell_exec("bash $file");
 echo "<pre>$output</pre>";
